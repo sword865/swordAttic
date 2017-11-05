847b8c9672f7a8f16eca98f3ce14d057da16f931
sword865 <sword865@163.com>
2017-11-05 22:57:47 +0800

    fix latex

--- src/Handler/Post.hs
+++ src/Handler/Post.hs
@@ -19,7 +19,6 @@ getPostR postId= do
     defaultLayout $ do
         let (commentFormId, commentTextareaId, commentUserId, commentUserEmailId, commentUserSiteId) = commentIds
         setTitle . toHtml $ "悟剑阁" <> "-" <> postTitle post
-        addScriptRemote "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
         $(widgetFile "post")
 
 commentIds :: (Text, Text, Text, Text, Text)
