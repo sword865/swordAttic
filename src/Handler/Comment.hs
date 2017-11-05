94b50c6d88cb4c01e46e40722b5f9b6161509758
sword865 <sword865@163.com>
2017-11-05 13:10:47 +0800

    post comment ok

--- src/Handler/Comment.hs
+++ src/Handler/Comment.hs
@@ -1,12 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Handler.Comment where
 
 import Import
+import Data.Time.Clock(getCurrentTime)
+import qualified Data.Text as T
+import Text.Markdown
 
 postCommentR :: Handler Value
 postCommentR = do
     -- requireJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
     -- (The ToJSON and FromJSON instances are derived in the config/models file).
+    liftIO $ Prelude.putStrLn "haha"
     comment <- (requireJsonBody :: Handler Comment)
-
-    insertedComment <- runDB $ insertEntity comment
+    liftIO $ Prelude.putStrLn "haha2"
+    now <- liftIO $ getCurrentTime
+    let comment' = comment { commentPosted = Just now }
+    liftIO $ Prelude.putStrLn "haha3"
+    insertedComment <- runDB $ insertEntity comment'
     returnJson insertedComment
+
+--
+-- getCommentR :: Handler Value
+-- getCommentR = do
+--     comment <- runDB $ selectList ([] :: [Filter Comment]) []
+--     returnJson comment
+
--- src/Handler/Post.hs
+++ src/Handler/Post.hs
@@ -6,7 +6,9 @@
 module Handler.Post where
 
 import Import
-
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import Text.Julius (RawJS (..))
+import Database.Persist.Sql(fromSqlKey)
 
 getPostR :: PostId -> Handler Html
 getPostR postId= do
@@ -15,6 +17,10 @@ getPostR postId= do
         comments <- selectList [CommentPost ==. postId] [Desc CommentPosted]
         return (post, comments)
     defaultLayout $ do
+        let (commentFormId, commentTextareaId, commentUserId, commentUserEmailId) = commentIds
         setTitle . toHtml $ "悟剑阁" <> "-" <> postTitle post
         addScriptRemote "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
         $(widgetFile "post")
+
+commentIds :: (Text, Text, Text, Text)
+commentIds = ("js-commentForm", "js-createCommentTextarea", "js-createCommentUser", "js-createCommentUserEmail")
